c4ed76f052066c0c210ff14ce1722bcf5990c50e
alberto cruz mtz <albertocruz8133@proton.me>
2026-07-29 15:44:53 -0600

    chore(auth): remove unused PrimaryKeyJoinColumn import from UserEntity

--- src/main/java/tienda/uni/api/auth/persistence/entity/UserEntity.java
+++ src/main/java/tienda/uni/api/auth/persistence/entity/UserEntity.java
@@ -12,7 +12,6 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
