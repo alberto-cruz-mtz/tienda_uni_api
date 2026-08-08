6de8be8aaa2fff4629b653fdb24f824066f509e0
alberto cruz mtz <albertocruz8133@proton.me>
2026-08-07 22:30:00 -0600

    feat(post): add findByIdAndUniversityId with eager fetch

--- src/main/java/tienda/uni/api/post/persistence/repository/PublicationRepository.java
+++ src/main/java/tienda/uni/api/post/persistence/repository/PublicationRepository.java
@@ -1,14 +1,16 @@
 package tienda.uni.api.post.persistence.repository;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tienda.uni.api.post.persistence.entity.PublicationEntity;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PublicationRepository extends JpaRepository<PublicationEntity, UUID>, JpaSpecificationExecutor<PublicationEntity> {
 
-    Slice<PublicationEntity> findAllBySalePerson_User_University_Id(UUID salePersonUserUniversityId, Pageable pageable);
+    @Query("SELECT post FROM PublicationEntity post JOIN FETCH post.salePerson sp JOIN FETCH sp.user u JOIN FETCH u.university uni WHERE post.id = :id AND uni.id = :universityId")
+    Optional<PublicationEntity> findByIdAndUniversityId(@Param("id") UUID id, @Param("universityId") UUID universityId);
 }
